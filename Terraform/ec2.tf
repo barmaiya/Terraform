f1109ed3d0587b194f14cfed9ff5910b4f1c505b
barmaiya <dev007mail@gmail.com>
2021-10-11 15:31:38 +0530

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,27 +1,22 @@
 #setup provider which is aws in this case with aws credential
 provider "aws"{
     region="ap-south-1"
-  
 }
 
 #create ec2 instance
 resource "aws_instance" "Node1"{
     ami ="ami-041d6256ed0f2061c"
     instance_type="t2.micro"
-    region  = "ap-south-1"
 }
 
+#create ec2 instance
+resource "aws_instance" "Node2"{
+    ami ="ami-041d6256ed0f2061c"
+    instance_type="t2.micro"
+}
 
-# #create ec2 instance
-# resource "aws_instance" "Node2"{
-#     ami ="ami-041d6256ed0f2061c"
-#     type="t2.micro"
-#     region  = "ap-south-1"
-# }
-
-# #create ec2 instance
-# resource "aws_instance" "Node3"{
-#     ami =""
-#     type="t2.micro"
-#     region  = "ap-south-1"
-# }
+#create ec2 instance
+resource "aws_instance" "Node3"{
+    ami ="ami-041d6256ed0f2061c"
+    instance_type="t2.micro"
+}
